ef658b7d6c690a75b2d2f72131b2121413b50cf6
AWAVAUATVWUSH <agent.maine.the.meta12@gmail.com>
2024-11-07 08:38:11 +0100

    Task 2

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import random
+
 def get_even_num():
     while(True):
         try:
@@ -8,4 +10,21 @@ def get_even_num():
                 print("A megadott szám nem páros. Páros számot kérek: ")
         except ValueError:
             print("Helytelen input!")
-            
+
+def randnum_gen(numOfNums:int = 0):
+    numlist:list = []
+    i:int = 0
+    while(i<numOfNums):
+        numlist.append(int(random.random()*161-10))
+        i+=1
+    return numlist
+
+def get_num_of_devis_by_three_in_list(inlist:list = []):
+    numToRet:int = 0
+    i:int = 0
+    iMax:int = len(inlist)
+    while(i<iMax):
+        if(inlist[i]%3==0):
+            numToRet+=1
+        i+=1
+    return numToRet
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from functions import get_even_num
+from functions import get_even_num, randnum_gen, get_num_of_devis_by_three_in_list
 
-print("Feldadat 1:\n")
+print("Feldadat 1:")
 num1 = get_even_num()
+
+print("\nFeldadat 2:")
+numlist:list = randnum_gen(13)
+print(numlist)
+devisByThree:int = get_num_of_devis_by_three_in_list(numlist)
+print(f"A számok között {devisByThree} db 3-mal osztható van!")
+
